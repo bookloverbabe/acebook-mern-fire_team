acd9d31ee3e7aeb8831a0413de7f8259fae1e7b4
kjartanh <kheimission@gmail.com>
2023-05-19 14:26:09 +0100

    css for homepage and navbar

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -1,47 +1,41 @@
 import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import './nav.css'
 import NavButton from '../navbutton/NavButton';
 
 // import logged in context as defined in App.js
-import {loggedInContext} from '../app/App';
-import { useNavigate } from "react-router";
+import { loggedInContext } from '../app/App';
 
 const NavBar = () => {
+  const [loggedIn, setLoggedIn] = useContext(loggedInContext);
+  const navigate = useNavigate();
 
-// set consts for loggedIn and setLoggedIn (funct) to be accessed within the component
-const [loggedIn, setLoggedIn] = useContext(loggedInContext)
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    setLoggedIn(false);
+    navigate('/login');
+  };
 
-const navigate = useNavigate()
-
-const logout = () => {
-  window.localStorage.removeItem("token")
-  setLoggedIn(false)
-  navigate('/login')
-}
-
-    return (
-      <>
-      <div className="nav-container">
-          {(loggedIn ?
-          <div className="nav-box">
-            <h1><i>Acebook</i></h1>
-            <NavButton to="/posts" value="Posts"/>
-            <NavButton to="/account" value="Your Account"/>
-            <button className="logout-button" onClick={logout}>Logout</button>
+  return (
+    <div className="nav-container">
+      {loggedIn ? (
+        <div className="nav-box">
+          <Link to="/posts" className="logo"><h1><i>Acebook</i></h1></Link>
+          <NavButton to="/posts" value="Posts" />
+          <NavButton to="/account" value="Your Account" />
+          <button className="logout-button" onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <div className="nav-box">
+          <Link to="/" className="logo"><h1><i>Acebook</i></h1></Link>
+          <div>
+            <NavButton className="signup-button" to="/signup" value="Sign-up" />
+            <NavButton className="login-button" to="/login" value="Login" />
           </div>
-          :
-          <div className="nav-box">
-            <h1><i>AceBook</i></h1>
-            <div>
-              <NavButton className="signup-button" to="/signup" value="Sign-up"/>
-
-              <NavButton className="login-button" to="/login" value="Login"/>
-            </div>
-          </div>
-          )}
-      </div>
-      </>
-    );
-}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default NavBar;
